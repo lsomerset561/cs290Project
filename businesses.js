eec710d77547a43c6e5de1701c2d1dae6c2a62bd
Lindsey Somerset <somersel@oregonstate.edu>
2017-11-21 15:26:08 -0500

    separate js for search.html & submit.html

--- businesses.js
+++ businesses.js
@@ -1,19 +1,3 @@
-//disable form submit if there are invalid fields
-(function () {
-	if(window.location.pathname === "submit.html"){
-		window.addEventListener('load', function () {
-			var businessForm = document.getElementById('needsValidation');
-			businessForm.addEventListener('submit', function (event) {
-				if (businessForm.checkValidity() === false) {
-					event.preventDefault();
-					event.stopPropagation();
-				}
-				businessForm.classList.add('was-validated');
-			}, false);
-		}, false);
-	}
-})();
-
 function Business(name, address, city, phone, email, type, logo, menu){
 	this.name = name;
 	this.address = address;
